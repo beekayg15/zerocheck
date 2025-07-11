c2c18e51e7dfcf6ed0d1851ebe4c6204e03a6daa
Barath Ganesh Kumar <barathganeshkumar2002@gmail.com>
2025-07-11 15:03:08 -0700

    switch to SHA256

--- src/pcs/univariate_pcs/ligero/data_structures.rs
+++ src/pcs/univariate_pcs/ligero/data_structures.rs
@@ -1,4 +1,4 @@
-use crate::pcs::univariate_pcs::ligero::FieldsToBytesHasher;
+use super::sha256_config::Sha256FieldsToBytesHasher;
 use ark_crypto_primitives::sponge::Absorb;
 use ark_ff::PrimeField;
 use ark_poly_commit::linear_codes::{LinCodePCCommitment, LinCodePCCommitmentState};
@@ -9,5 +9,5 @@ use super::merkle_config::MerkleConfig;
 #[derive(Clone, Debug, CanonicalSerialize, CanonicalDeserialize)]
 pub struct Commitment<F: PrimeField + Absorb> {
     pub(crate) commitment: LinCodePCCommitment<MerkleConfig<F>>,
-    pub(crate) state: LinCodePCCommitmentState<F, FieldsToBytesHasher<F>>,
+    pub(crate) state: LinCodePCCommitmentState<F, Sha256FieldsToBytesHasher<F>>,
 }
