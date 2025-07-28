7b39b7334f7ab22756df427cd3eb894951cead03
KenKenNeed <2317150243@qq.com>
2025-07-28 15:38:48 -0400

    Update testing script

--- bin/run_bins.sh
+++ bin/run_bins.sh
@@ -40,61 +40,11 @@ MAX_SIZE=16
 # CMDS1="RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=18 --max-size=18 --prepare-threads=64 --run-threads=1 | tee -a output_log/univar_opt_bench_multhr_64.log "
 # CMDS1="cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=18 --max-size=18 --prepare-threads=64 --run-threads=1 --batch-opening-threads=4 | tee -a output_log/univar_opt_bench_run_1_open_4.log "
 # CMDS1="cargo run --release --bin univar_opt_bench_multhr -- --repeat=1 --min-size=26 --max-size=28 --prepare-threads=64 --run-threads=1 --batch-opening-threads=5 | tee -a output_log/univar_opt_bench_1_26_28_run_1_open_5.log "
-# CMDS1="cargo run --release --bin univar_opt_bench_multhr -- --repeat=1 --min-size=18 --max-size=18 --prepare-threads=64 --run-threads=1 --batch-opening-threads=8 | tee -a output_log/univar_opt_bench_run_1_open_8.log "
+CMDS1="cargo run --release --bin univar_opt_bench_multhr -- --repeat=1 --min-size=8 --max-size=8 --prepare-threads=64 --poly-commit-scheme=kzg_offloaded --run-threads=1 --batch-opening-threads=1 | tee -a output_log/univar_opt_bench_run_1_open_8.log "
 
 # CMDS1="cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=18 --max-size=18 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=hyrax --batch-opening-threads=1 | tee -a output_log/mullin_opt_bench_run_1_open_1.log "
 # CMDS1="cargo run --release --bin mullin_opt_bench_multhr -- --repeat=1 --min-size=26 --max-size=28 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=hyrax --batch-opening-threads=4 | tee -a output_log/mullin_opt_bench_1_26_28_run_1_open_4.log "
 
 
-# echo "Running: $CMDS1"
-# eval "$CMDS1"
-
-############################################################
-####### multi-linear (hyrax) #######
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=hyrax --batch-opening-threads=4"; RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=hyrax --batch-opening-threads=4; } > output_log/mullin_opt_bench_16_16_run_1_hyrax_open_4.log 2>&1
-
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=hyrax --batch-opening-threads=4"; RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=hyrax --batch-opening-threads=4; } > output_log/mullin_opt_bench_24_24_run_1_hyrax_open_4.log 2>&1
-
-{ echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=hyrax --batch-opening-threads=48"; RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=hyrax --batch-opening-threads=48; } > output_log/mullin_opt_bench_24_24_run_12_hyrax_open_48.log 2>&1
-
-####### multi-linear (kzg) #######
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=kzg --batch-opening-threads=4"; RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=kzg --batch-opening-threads=4; } > output_log/mullin_opt_bench_16_16_run_1_kzg_open_4.log 2>&1
-
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=kzg --batch-opening-threads=4"; RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=kzg --batch-opening-threads=4; } > output_log/mullin_opt_bench_24_24_run_1_kzg_open_4.log 2>&1
-
-{ echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=kzg --batch-opening-threads=48"; RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=kzg --batch-opening-threads=48; } > output_log/mullin_opt_bench_24_24_run_12_kzg_open_48.log 2>&1
-
-####### multi-linear (ligero) #######
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero --batch-opening-threads=4"; RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero --batch-opening-threads=4; } > output_log/mullin_opt_bench_16_16_run_1_ligero_open_4.log 2>&1
-
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero --batch-opening-threads=4"; RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero --batch-opening-threads=4; } > output_log/mullin_opt_bench_24_24_run_1_ligero_open_4.log 2>&1
-
-{ echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=ligero --batch-opening-threads=48"; RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=ligero --batch-opening-threads=48; } > output_log/mullin_opt_bench_24_24_run_12_ligero_open_48.log 2>&1
-
-####### multi-linear (ligero_poseidon) #######
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero_poseidon --batch-opening-threads=4"; RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero_poseidon --batch-opening-threads=4; } > output_log/mullin_opt_bench_16_16_run_1_ligeroposeidon_open_4.log 2>&1
-
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero_poseidon --batch-opening-threads=4"; RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero_poseidon --batch-opening-threads=4; } > output_log/mullin_opt_bench_24_24_run_1_ligeroposeidon_open_4.log 2>&1
-
-{ echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=ligero_poseidon --batch-opening-threads=48"; RAYON_NUM_THREADS=64 cargo run --release --bin mullin_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=ligero_poseidon --batch-opening-threads=48; } > output_log/mullin_opt_bench_24_24_run_12_ligeroposeidon_open_48.log 2>&1
-
-####### univar (kzg) #######
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=kzg --batch-opening-threads=5" ; RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=kzg --batch-opening-threads=5; } > output_log/univar_opt_bench_16_16_run_1_kzg_open_5.log 2>&1
-
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=kzg --batch-opening-threads=5" ; RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=kzg --batch-opening-threads=5; } > output_log/univar_opt_bench_24_24_run_1_kzg_open_5.log 2>&1
-
-{ echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=kzg --batch-opening-threads=60" ; RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=kzg --batch-opening-threads=60; } > output_log/univar_opt_bench_24_24_run_12_kzg_open_60.log 2>&1
-
-####### univar (ligero) #######
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero --batch-opening-threads=5" ; RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero --batch-opening-threads=5; } > output_log/univar_opt_bench_16_16_run_1_ligero_open_5.log 2>&1
-
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero --batch-opening-threads=5" ; RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero --batch-opening-threads=5; } > output_log/univar_opt_bench_24_24_run_1_ligero_open_5.log 2>&1
-
-{ echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=ligero --batch-opening-threads=60" ; RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=ligero --batch-opening-threads=60; } > output_log/univar_opt_bench_24_24_run_12_ligero_open_60.log 2>&1
-
-####### univar (ligero_poseidon) #######
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero_poseidon --batch-opening-threads=5" ; RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=1 --min-size=16 --max-size=16 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero_poseidon --batch-opening-threads=5; } > output_log/univar_opt_bench_16_16_run_1_ligeroposeidon_open_5.log 2>&1
-
-# { echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero_poseidon --batch-opening-threads=5" ; RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=1 --poly-commit-scheme=ligero_poseidon --batch-opening-threads=5; } > output_log/univar_opt_bench_24_24_run_1_ligeroposeidon_open_5.log 2>&1
-
-{ echo "Running: RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=ligero_poseidon --batch-opening-threads=60" ; RAYON_NUM_THREADS=64 cargo run --release --bin univar_opt_bench_multhr -- --repeat=2 --min-size=24 --max-size=24 --prepare-threads=64 --run-threads=12 --poly-commit-scheme=ligero_poseidon --batch-opening-threads=60; } > output_log/univar_opt_bench_24_24_run_12_ligeroposeidon_open_60.log 2>&1
+echo "Running: $CMDS1"
+eval "$CMDS1"
