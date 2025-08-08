f9f8019b23c11afdf9806c6ed90da81c86d67b3d
Alhad Daftardar <ajd9396@nyu.edu>
2025-08-08 12:58:00 -0400

    added multi-pe functionality for ntt simulator as well as testing scripts

---
+++ hardware_experiments/params_ntt_v_sum.py
@@ -0,0 +1,8 @@
+
+
+modmul_area = 0.478
+modadd_area = 0.002
+
+BITS_PER_GB = 1<<33
+BITS_PER_MB = 1<<23
+MB_CONVERSION_FACTOR = 1.76 # mm^2 / MB
---
+++ hardware_experiments/quick_test.sh
@@ -0,0 +1,4 @@
+
+python test_ntt_func_sim.py --n 16 --bw 4096 --mode one_config
+
+python test_ntt_func_sim.py --n 16 --bw 4096 --mode print
